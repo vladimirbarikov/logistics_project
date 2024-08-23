4ef8e3f56d8129305cd6d495449ea0ab0507f485
Vladimir Barikov <vladimir.barikov.85@gmail.com>
2024-08-23 23:29:08 +0300

    database with character set utf8 created. main, departments, branch, company and staff entities relationships created. data for entities main, department, branch and company loaded. data for entity staff must be loaded manually

--- database/pet_project.sql
+++ database/pet_project.sql
@@ -1,34 +1,95 @@
-/* begin database creation */
+/* begin database pet_project creation */
 
-create database pet_project;
+create database pet_project character set utf8;
 
-/* end database creation */
+/* end database pet_project creation */
 
 /* begin table creation */
 
 create table main
-(
-    main_id VARCHAR(255),
-    main_title VARCHAR(255),
-    constraint pk_main primary key (main_id)
-);
+ (main_id tinyint unsigned not null auto_increment,
+  title varchar(50) not null,
+  constraint pk_main primary key (main_id)
+ );
 
 create table departments
-(
-    dept_id smallint unsigned not null auto_increment,
-    main_id varchar(10),
-    department varchar(255),
-    constraint pk_departments primary key (dept_id, main_id)
-);
+ (dept_id tinyint unsigned not null auto_increment,
+  main_id tinyint unsigned not null,
+  department varchar(100),
+  constraint fk_departments_main foreign key (main_id) 
+  references main (main_id) on delete cascade on update restrict,
+  constraint pk_departments primary key (dept_id)
+ );
+
+create table branch
+ (branch_id tinyint unsigned not null auto_increment,
+  country varchar(100),
+  city varchar(100),
+  constraint pk_branch primary key (branch_id)
+ );
+
+create table company
+ (company_id tinyint unsigned not null auto_increment,
+  company_name varchar(100),
+  responsibility varchar(100),
+  constraint pk_company primary key (company_id)
+ );
+
+create table staff
+ (staff_id tinyint unsigned not null auto_increment,
+  dept_id tinyint unsigned not null,
+  f_name varchar(100),
+  l_name varchar(100),
+  phone varchar(50),
+  email varchar(100),
+  responsibility varchar(100),
+  company_id tinyint unsigned not null,
+  leader_id tinyint unsigned,
+  branch_id tinyint unsigned not null,
+  constraint fk_staff_staff foreign key (leader_id)
+  references staff (staff_id),
+  constraint fk_staff_departments foreign key (dept_id) 
+  references departments (dept_id) on delete cascade on update restrict,
+  constraint fk_staff_company foreign key (company_id) 
+  references company (company_id) on delete cascade on update restrict,
+  constraint fk_staff_branch foreign key (branch_id) 
+  references branch (branch_id) on delete cascade on update restrict,
+  constraint pk_staff primary key (staff_id)
+ );
 
 /* end table creation */
 
 /* begin data population */
 
-load data local infile '/Users/vladimirbarikov/github_repository/sql/pet_project/database/main.csv' 
+/* main data */
+load data local infile '/Users/vladimirbarikov/github/pet_project_rep/database/main.csv'
  into table main 
- fields terminated by ',' 
+ fields terminated by ';' 
  lines terminated by '\n' 
- ignore 1 rows (main_id, main_title);
+ ignore 1 rows (main_id, title);
+
+/* departments data */
+load data local infile '/Users/vladimirbarikov/github/pet_project_rep/database/departments.csv'
+ into table departments 
+ fields terminated by ';' 
+ lines terminated by '\n' 
+ ignore 1 rows (dept_id, main_id, department);
+
+/* branch data */
+load data local infile '/Users/vladimirbarikov/github/pet_project_rep/database/branch.csv'
+ into table branch
+ fields terminated by ';' 
+ lines terminated by '\n' 
+ ignore 1 rows (branch_id, country, city);
+
+/* company data */
+load data local infile '/Users/vladimirbarikov/github/pet_project_rep/database/company.csv'
+ into table company
+ fields terminated by ';' 
+ lines terminated by '\n' 
+ ignore 1 rows (company_id, company_name, responsibility);
+
+/* staff data */
+
 
 /* end data population */
