5355f353d82861973123a6afdcf2a06897ccca50
Vladimir Barikov <vladimir.barikov.85@gmail.com>
2024-08-29 17:43:36 +0300

    tables "main", "branch", "departments", "staff", "customers", "customer_contacts", "providers", "provider_contacts", "products" were created and modified

--- database/pet_project.sql
+++ database/pet_project.sql
@@ -8,65 +8,118 @@ use pet_project database
 
 /* begin tables creation */
 
-create table main
- (main_id tinyint unsigned not null auto_increment,
-  title varchar(15) not null,
-  constraint pk_main primary key (main_id)
- );
+create table main (
+    main_id tinyint unsigned not null auto_increment,
+    title varchar(30) not null,
+    constraint pk_main primary key (main_id)
+);
 
-create table branch
- (branch_id tinyint unsigned not null auto_increment,
-  country tinitext,
-  city varchar(30),
-  constraint pk_branch primary key (branch_id)
- );
+create table branch (
+    branch_id tinyint unsigned not null auto_increment,
+    main_id tinyint unsigned not null,
+    country tinytext,
+    city varchar(20),
+    constraint fk_branch_main foreign key (main_id)
+    references main (main_id) on delete cascade on update restrict,
+    constraint pk_branch primary key (branch_id)
+);
 
-create table departments
- (dept_id tinyint unsigned not null auto_increment,
-  main_id tinyint unsigned not null,
-  department tinytext,
-  constraint fk_departments_main foreign key (main_id) 
-  references main (main_id) on delete cascade on update restrict,
-  constraint pk_departments primary key (dept_id)
- );
+create table departments (
+    dept_id tinyint unsigned not null auto_increment,
+    main_id tinyint unsigned not null,
+    department tinytext,
+    constraint fk_departments_main foreign key (main_id)
+    references main (main_id) on delete cascade on update restrict,
+    constraint pk_departments primary key (dept_id)
+);
 
-create table staff
- (staff_id tinyint unsigned not null auto_increment,
-  dept_id tinyint unsigned not null,
-  f_name varchar(30),
-  l_name varchar(30),
-  phone varchar(30),
-  email varchar(50),
-  responsibility varchar(50) not null,
-  company_id tinyint unsigned not null,
-  leader_id tinyint unsigned,
-  branch_id tinyint unsigned not null,
-  constraint fk_staff_staff foreign key (leader_id)
-  references staff (staff_id),
-  constraint fk_staff_departments foreign key (dept_id) 
-  references departments (dept_id) on delete cascade on update restrict,
-  constraint fk_staff_company foreign key (company_id) 
-  references company (company_id) on delete cascade on update restrict,
-  constraint fk_staff_branch foreign key (branch_id) 
-  references branch (branch_id) on delete cascade on update restrict,
-  constraint pk_staff primary key (staff_id)
- );
+create table staff (
+    staff_id tinyint unsigned not null auto_increment,
+    dept_id tinyint unsigned not null,
+    main_id tinyint unsigned not null,
+    f_name tinytext,
+    l_name tinytext,
+    phone varchar(30),
+    email varchar(50),
+    responsibility tinytext not null,
+    leader_id tinyint unsigned,
+    branch_id tinyint unsigned not null,
+    constraint fk_staff_main foreign key (main_id)
+    references main (main_id) on delete cascade on update restrict,
+    constraint fk_staff_branch foreign key (branch_id)
+    references branch (branch_id) on delete cascade on update restrict,
+    constraint fk_staff_departments foreign key (dept_id)
+    references departments (dept_id) on delete cascade on update restrict,
+    constraint fk_staff_staff foreign key (leader_id)
+    references staff (staff_id) on delete cascade on update restrict,
+    constraint pk_staff primary key (staff_id)
+);
 
-create table customers
- (customer_id tinyint unsigned not null auto_increment,
-  staff_id tinyint unsigned not null,
-  customer_name varchar(30),
-  city varchar(30),
-  street varchar(30),
-  building varchar(10),
-  contact_person varchar(30),
-  contact_phone varchar(30),
-  work_start varchar(5),
-  work_end varchar(5),
-  constraint fk_customers_staff foreign key (staff_id)
-  references staff (staff_id),
-  constraint pk_customers primary key (customer_id)
- );
+create table customers (
+    customer_id tinyint unsigned not null auto_increment,
+    staff_id tinyint unsigned not null,
+    main_id tinyint unsigned not null,
+    customer_name varchar(50),
+    country tinytext,
+    city varchar(20),
+    street varchar(30),
+    building varchar(10),
+    work_start varchar(5),
+    work_end varchar(5),
+    constraint fk_customers_main foreign key (main_id)
+    references main (main_id) on delete cascade on update restrict,
+    constraint fk_customers_staff foreign key (staff_id)
+    references staff (staff_id) on delete cascade on update restrict,
+    constraint pk_customers primary key (customer_id)
+);
+
+create table customer_contacts (
+   contact_id tinyint unsigned not null auto_increment,
+   customer_id tinyint unsigned not null,
+   f_name tinytext not null,
+   l_name tinytext,
+   phone varchar(30) not null,
+   email varchar(50), 
+   responsibility tinytext,
+   constraint fk_customer_contacts_customers foreign key (customer_id)
+   references customers (customer_id) on delete cascade on update restrict,
+   constraint pk_customer_contacts primary key (contact_id)
+
+);
+
+create table providers (
+    provider_id tinyint unsigned not null auto_increment,
+    main_id tinyint unsigned not null,
+    provider_name varchar(50) not null,
+    responsibility tinytext not null,
+    constraint fk_providers_main foreign key (main_id)
+    references main (main_id)on delete cascade on update restrict,
+    constraint pk_providers primary key (provider_id)
+);
+
+create table provider_contacts (
+   contact_id tinyint unsigned not null auto_increment,
+   provider_id tinyint unsigned not null,
+   f_name tinytext not null,
+   l_name tinytext,
+   phone varchar(30) not null,
+   email varchar(50),
+   responsibility tinytext not null,
+   leader_id tinyint unsigned,
+   constraint fk_provider_contacts_providers foreign key (provider_id)
+   references providers (provider_id) on delete cascade on update restrict,
+   constraint pk_provider_contacts primary key (contact_id)
+);
+
+create table products (
+    product_id tinyint unsigned not null auto_increment,
+    main_id tinyint unsigned not null,
+    product tinytext not null,
+    description tinytext,
+    constraint fk_products_main foreign key (main_id)
+    references main (main_id) on delete cascade on update restrict,
+    constraint pk_products primary key (product_id)
+);
 
 /* end table creation */
 
